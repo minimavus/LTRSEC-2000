ba31606978ee4e0390550cda4a246a197bf8b3f0
minimavus <jedubois@cisco.com>
2025-01-06 19:06:05 +0000

    VPC Configuration Done

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -31,3 +31,47 @@ resource "aws_vpc" "pod-vpc" {
     Created = "${local.formatted_date}"
   }
 }
+
+#Create Private Subnet
+resource "aws_subnet" "pod-private-subnet" {
+  vpc_id     = aws_vpc.pod-vpc.id
+  cidr_block = "10.${var.pod_number}.0.0/24"
+
+  tags = {
+    Name = "ISEinAWS-pod${var.pod_number}_Private_Subnet"
+    Created = "${local.formatted_date}"
+  }
+}
+
+#Create Transit Gateway Attachment
+resource "aws_ec2_transit_gateway_vpc_attachment" "zer0k-transit-gateway" {
+  subnet_ids         = [aws_subnet.pod-private-subnet.id]
+  transit_gateway_id = "${var.zer0k_transit_gateway}"
+  vpc_id             = aws_vpc.pod-vpc.id
+
+  tags = {
+    Name = "ISEinAWS-pod${var.pod_number}_Transit_Attach"
+    Created = "${local.formatted_date}"
+  }
+}
+
+#Create Private Route Table
+resource "aws_route_table" "pod-private-rt" {
+  vpc_id = aws_vpc.pod-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    transit_gateway_id = "${var.zer0k_transit_gateway}"
+  }
+
+  tags = {
+    Name = "ISEinAWS-pod${var.pod_number}_Transit_Attach"
+    Created = "${local.formatted_date}"
+  }
+}
+
+#Attach the Route Table to the Subnet
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.pod-private-subnet.id
+  route_table_id = aws_route_table.pod-private-rt.id
+}
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -10,3 +10,28 @@ variable "pod_number" {
         type = string
         default = "0"
 }
+
+#Lab Environment Variables
+variable "zer0k_transit_gateway" {
+        description = "Transit Gateway ID"
+        type = string
+        default = "tgw-00ecf6a9a26ff37cf"
+}
+
+variable "zer0k_private_subnet" {
+        description = "Zer0k Private Subnet CIDR"
+        type = string
+        default = "172.18.16.0/21"
+}
+
+variable "zer0k_inside_subnet" {
+        description = "Zer0k Inside Subnet CIDR"
+        type = string
+        default = "172.18.24.0/21"
+}
+
+variable "zer0k_vpn_subnet" {
+        description = "Zer0k VPN Subnet CIDR"
+        type = string
+        default = "172.16.1.0/24"
+}
